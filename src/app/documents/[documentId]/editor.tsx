23243cc200663e52fd58664a829d06f56fc9171d
zntb <zenetibi@yahoo.com>
2025-04-01 11:14:36 +0300

    Add Color tools (text & highlight)

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -10,6 +10,10 @@ import TableHeader from '@tiptap/extension-table-header';
 import TableRow from '@tiptap/extension-table-row';
 import Image from '@tiptap/extension-image';
 import Underline from '@tiptap/extension-underline';
+import FontFamily from '@tiptap/extension-font-family';
+import TextStyle from '@tiptap/extension-text-style';
+import Highlight from '@tiptap/extension-highlight';
+import { Color } from '@tiptap/extension-color';
 import ImageResize from 'tiptap-extension-resize-image';
 
 import { useEditorStore } from '@/store/use-editor-store';
@@ -58,7 +62,11 @@ export const Editor = () => {
       TaskList,
       Image,
       Underline,
+      FontFamily,
+      TextStyle,
+      Color,
       ImageResize,
+      Highlight.configure({ multicolor: true }),
       TaskItem.configure({
         nested: true,
       }),
--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -3,8 +3,12 @@
 import { Separator } from '@/components/ui/separator';
 import { cn } from '@/lib/utils';
 import { useEditorStore } from '@/store/use-editor-store';
+import { type Level } from '@tiptap/extension-heading';
+import { type ColorResult, SketchPicker } from 'react-color';
 import {
   BoldIcon,
+  ChevronDownIcon,
+  HighlighterIcon,
   ItalicIcon,
   ListTodoIcon,
   LucideIcon,
@@ -16,6 +20,163 @@ import {
   UnderlineIcon,
   Undo2Icon,
 } from 'lucide-react';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+
+const HighlightColorButton = () => {
+  const { editor } = useEditorStore();
+
+  const value = editor?.getAttributes('highlight').color || '#FFFFFF';
+
+  const onChange = (color: ColorResult) => {
+    editor?.chain().focus().setHighlight({ color: color.hex }).run();
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className='h-7 min-w-7 shrink-0 flex flex-col items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm'>
+          <HighlighterIcon className='size-4' />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className='p-0'>
+        <SketchPicker color={value} onChange={onChange} />
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+const TextColorButton = () => {
+  const { editor } = useEditorStore();
+
+  const value = editor?.getAttributes('textStyle')?.color || '#000000';
+
+  const onChange = (color: ColorResult) => {
+    editor?.chain().focus().setColor(color.hex).run();
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className='h-7 min-w-7 shrink-0 flex flex-col items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm'>
+          <span className='text-xs'>A</span>
+          <div
+            className='h-0.5 w-full'
+            style={{ backgroundColor: value }}
+          ></div>
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className='p-0'>
+        <SketchPicker color={value} onChange={onChange} />
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+const HeadingLevelButton = () => {
+  const { editor } = useEditorStore();
+
+  const headings = [
+    { label: 'Normal text', value: 0, fontSize: '16px' },
+    { label: 'Heading 1', value: 1, fontSize: '32px' },
+    { label: 'Heading 2', value: 2, fontSize: '24px' },
+    { label: 'Heading 3', value: 3, fontSize: '20px' },
+    { label: 'Heading 4', value: 4, fontSize: '18px' },
+    { label: 'Heading 5', value: 5, fontSize: '16px' },
+  ];
+
+  const getCurrentHeading = () => {
+    for (let level = 1; level <= 5; level++) {
+      if (editor?.isActive('heading', { level })) {
+        return `Heading ${level}`;
+      }
+    }
+
+    return 'Normal text';
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className='h-7 min-w-7 shrink-0 flex items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm'>
+          <span className='truncate'>{getCurrentHeading()}</span>
+          <ChevronDownIcon className='ml-2 size-4 shrink-0' />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className='p-1 flex flex-col gap-y-1'>
+        {headings.map(({ label, value, fontSize }) => (
+          <button
+            key={value}
+            className={cn(
+              'flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80',
+              (value === 0 && !editor?.isActive('heading')) ||
+                (editor?.isActive('heading', { level: value as Level }) &&
+                  'bg-neutral-200/80'),
+            )}
+            style={{ fontSize }}
+            onClick={() => {
+              if (value === 0) {
+                editor?.chain().focus().setParagraph().run();
+              } else {
+                editor
+                  ?.chain()
+                  .focus()
+                  .toggleHeading({ level: value as Level })
+                  .run();
+              }
+            }}
+          >
+            {label}
+          </button>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+const FontFamilyButton = () => {
+  const { editor } = useEditorStore();
+
+  const fonts = [
+    { label: 'Arial', value: 'Arial' },
+    { label: 'Times New Roman', value: 'Times New Roman' },
+    { label: 'Courier New', value: 'Courier New' },
+    { label: 'Georgia', value: 'Georgia' },
+    { label: 'Verdana', value: 'Verdana' },
+  ];
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className='h-7 w-[120px] shrink-0 flex items-center justify-between rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm'>
+          <span className='truncate'>
+            {editor?.getAttributes('textStyle')?.fontFamily || 'Arial'}
+          </span>
+          <ChevronDownIcon className='ml-2 size-4 shrink-0' />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className='p-1 flex flex-col gap-y-1'>
+        {fonts.map(({ label, value }) => (
+          <button
+            key={value}
+            className={cn(
+              'flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80',
+              editor?.getAttributes('textStyle').FontFamily === value &&
+                'bg-neutral-200/80',
+            )}
+            style={{ fontFamily: value }}
+            onClick={() => editor?.chain().focus().setFontFamily(value).run()}
+          >
+            <span className='text-sm'>{label}</span>
+          </button>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
 
 interface ToolbarButtonProps {
   onClick?: () => void;
@@ -125,17 +286,17 @@ export const Toolbar = () => {
         <ToolbarButton key={item.label} {...item} />
       ))}
       <Separator orientation='vertical' className='h-6 bg-neutral-300' />
-      {/* TODO: Add font family */}
+      <FontFamilyButton />
       <Separator orientation='vertical' className='h-6 bg-neutral-300' />
-      {/* TODO: Add Heading */}
+      <HeadingLevelButton />
       <Separator orientation='vertical' className='h-6 bg-neutral-300' />
       {/* TODO: Add Font size */}
       <Separator orientation='vertical' className='h-6 bg-neutral-300' />
       {sections[1].map(item => (
         <ToolbarButton key={item.label} {...item} />
       ))}
-      {/* TODO: Add text color */}
-      {/* TODO: Add highlight color */}
+      <TextColorButton />
+      <HighlightColorButton />
       <Separator orientation='vertical' className='h-6 bg-neutral-300' />
       {/* TODO: Add link */}
       {/* TODO: Add image */}
